3a70c7dec3b7abc7e6356dbc04d26f47fa232bdd
Adam Palmer <ad@palmerama.tv>
2012-12-04 17:45:51 +0000

    Pit changes
    Signed-off-by: Adam Palmer <ad@palmerama.tv>

--- js/game/Pit.js
+++ js/game/Pit.js
@@ -3,39 +3,73 @@
     var namespace = GAMEMAIN.namespace('GAMEMAIN.game');
 
     var stage;
-
+    var horsesManager;
 
 
     if (namespace.Pit === undefined) 
 	{
-        namespace.Pit = function(aStage)
+        namespace.Pit = function(aStage, aHorsesManager)
  		{	
 			stage = aStage;
+			horsesManager = aHorsesManager;
         }
 
         var p = namespace.Pit.prototype;
 		
 		
-		p.init = function(horse)
+		p.init = function(id, holeMaskPos, ballXPos, doorProps, lanes)
 		{
-			this.horse = horse;
+			this.id = id;
+			this.horse = horsesManager['horse'+id];
+			this.holeMaskPos = holeMaskPos;
+			this.ballX = ballXPos;
+			this.doorProps = doorProps;
+			this.lanes = lanes;
 
-			this.ballMask = new createjs.Bitmap('img/pit3mask.png');
-			this.ballMask.x = 377;
-			this.ballMask.y = 346;
-			stage.addChild(this.ballMask);
+			this.createMasks();
+			this.createDoors();
+			this.createBall();
+		}
 
+		p.createBall = function()
+		{
 			this.ball = new createjs.Bitmap('img/ball.png');
 			this.ball.regX = 22;
 			this.ball.regY = 37;
-			this.ball.x = 400;
-			this.ball.y = 200;
+			this.ball.x = -1000;
+			this.ball.y = -1000;
 			stage.addChild(this.ball);
+		}
+
+		p.createMasks = function()
+		{
+			this.holeMask = new createjs.Bitmap('img/pit' + this.id + 'mask.png');
+			this.holeMask.x = this.holeMaskPos.x;
+			this.holeMask.y = this.holeMaskPos.y;
+			stage.addChild(this.holeMask);
+		}
+
+		p.createDoors = function()
+		{
+			this.leftDoor = new createjs.Bitmap('img/pitdoor.png');
+			this.leftDoor.skewX = this.doorProps.skew;
+			this.leftDoor.x = this.doorProps.x - this.leftDoor.image.width;
+			this.leftDoor.y = 347 - this.leftDoor.image.height/2;
+			stage.addChildAt(this.leftDoor, stage.getChildIndex(this.lanes));
+
+			this.rightDoor = this.leftDoor.clone();
+			this.rightDoor.skewX = this.doorProps.skew;
+			this.rightDoor.x = this.doorProps.x;
+			this.rightDoor.y = 347 - this.rightDoor.image.height/2;
+			stage.addChildAt(this.rightDoor, stage.getChildIndex(this.lanes));
+
+			this.doorsOpen = false;
 		}
 
 		p.startBall = function()
 		{
 			stage.addChild(this.ball);
+			this.ball.x = this.ballX.startX;
 			this.ball.y = 550;
 			this.ball.scaleX = this.ball.scaleY = 1.3;
 			TweenLite.to(this.ball, .4, {delay:1, y:472, scaleX:1, scaleY:1, ease:Quad.easeOut, onComplete:this.addPressListenerToBall.bind(this)});
@@ -49,14 +83,38 @@
 		p.onPressBall = function(evt)
 		{
 			evt.target.onPress = null;
-			TweenLite.to(evt.target, .45, {y:355, scaleX:.75, scaleY:.75, ease:Sine.easeIn, onComplete:this.potBall.bind(this)});
+			this.ballTween = TweenLite.to(evt.target, .45, {x:this.ballX.endX, y:328, scaleX:.75, scaleY:.75, ease:Sine.easeIn, 
+				onUpdate:this.onBallUpdate.bind(this), onComplete:this.potBall.bind(this)});
+		}
+
+		p.onBallUpdate = function()
+		{
+			if (this.ball.y <= 355 && this.doorsOpen) 
+			{
+				this.ballTween.kill();
+				this.potBall();
+			}
 		}
 
 		p.potBall = function()
 		{
-			stage.setChildIndex(this.ballMask, stage.getChildIndex(this.ball));			
+			stage.setChildIndex(this.holeMask, stage.getChildIndex(this.ball));			
 			TweenLite.to(this.ball, .3, {y:400, onComplete:this.startBall.bind(this)});
-			this.horse.moveForward();
+			if (this.doorsOpen) this.horse.moveForward();
+		}
+
+		p.openDoors = function()
+		{
+			this.doorsOpen = true;
+			TweenLite.to(this.leftDoor, .5, {x:this.doorProps.x - this.leftDoor.image.width*2, ease:Bounce.easeOut});
+			TweenLite.to(this.rightDoor, .5, {x:this.doorProps.x + this.rightDoor.image.width, ease:Bounce.easeOut});
+		}
+
+		p.closeDoors = function()
+		{
+			this.doorsOpen = false;
+			TweenLite.to(this.leftDoor, .5, {x:this.doorProps.x - this.leftDoor.image.width, ease:Bounce.easeOut});
+			TweenLite.to(this.rightDoor, .5, {x:this.doorProps.x, ease:Bounce.easeOut});
 		}
 	}
 
--- js/game/PitsManager.js
+++ js/game/PitsManager.js
@@ -4,6 +4,7 @@
 
     var stage;
     var horsesManager;
+    var difficulty;
 
 
 
@@ -13,6 +14,7 @@
  		{	
 			stage = aStage;
 			horsesManager = aHorsesManager;
+			difficulty = 'hard';
         }
 
         var p = namespace.PitsManager.prototype;
@@ -23,10 +25,54 @@
 			var lanes = new createjs.Bitmap('img/lanes.png');
 			lanes.y = 265;
 			stage.addChild(lanes);
+			
+			this.pits = [];
 
-			this.pit3 = new namespace.Pit(gameStage);
-			this.pit3.init(horsesManager.horse3);
-			this.pit3.startBall();
+			var pit3 = new namespace.Pit(gameStage, horsesManager);
+			pit3.init(3, {x:377, y:346}, {startX:400, endX:400}, {x:400, skew:0}, lanes);
+			this.pits.push(pit3);
+
+			pit3.startBall();
+			this.startDoorsSequence();
+		}
+
+		p.startDoorsSequence = function()
+		{
+			this.doorsSequence = [1, 2, 1, .5, 2, 1];
+			this.doorsPointer = -1;
+			this.doNextStep();
+		}
+
+		p.doNextStep = function()
+		{
+			if (++this.doorsPointer >= this.doorsSequence.length) this.doorsPointer = 0;
+
+			for (var i=0; i<this.pits.length; ++i) 
+			{
+				this.pits[i].doorsOpen ? this.pits[i].closeDoors() : this.pits[i].openDoors();
+			}
+
+			var delayMultiplier; 
+
+			switch (difficulty) 
+			{
+				case 'easy':
+					delayMultiplier = 3;
+					break;
+
+				case 'medium':
+					delayMultiplier = 2;
+					break;
+
+				case 'hard':
+					delayMultiplier = 1;
+					break;
+			}
+
+			var delay = this.doorsSequence[this.doorsPointer] * delayMultiplier;
+			TweenLite.delayedCall(delay, this.doNextStep.bind(this));
+
+			console.log('doors step: ' + this.doorsPointer + ', delay: ' + delay + ' secs');
 		}
 }
 
